705beb0eaa30e8917cbea6bf57515cc698867ab0
Ayman El Jamaaouy <ael-jama@e1r2p10.1337.ma>
2025-06-23 10:24:04 +0100

    the last commit

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 /*   By: ael-jama <ael-jama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 17:05:56 by ael-jama          #+#    #+#             */
-/*   Updated: 2025/06/19 17:49:42 by ael-jama         ###   ########.fr       */
+/*   Updated: 2025/06/23 10:23:47 by ael-jama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	*one_philo(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	printf("0 1 is thinking\n");
+	usleep(philo->data->time_to_die * 1000);
+	printf("%d 1 died\n", philo->data->time_to_die);
+	return (NULL);
+}
 
 int	main(int ac, char **av)
 {
@@ -24,11 +35,12 @@ int	main(int ac, char **av)
 		return (write(2, "error, memory allocation\n", 26), free_data(data), 1);
 	if (data->num_philos == 1)
 	{
-		printf("0 1 is thinking\n");
-		usleep(data->time_to_die * 1000);
-		printf("%d 1 died\n", data->time_to_die);
-		free_data(data);
-		return (0);
+		if (pthread_create(&data->philos[0].thread, NULL, one_philo,
+				&data->philos[0]) != 0)
+			return (write(2, "Error: Failed to create philosopher thread\n",
+					44), 1);
+		pthread_join(data->philos[0].thread, NULL);
+		return (free_data(data), 0);
 	}
 	if (data->num_times_must_eat == 0)
 		return (printf("all philosophers have finished their meals\n"),
